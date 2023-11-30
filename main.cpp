ef9b6529da60139fc817c1d2730e9098f30da41a
bornabhowmik <bornabhowmik4@gmail.com>
2023-11-30 18:49:49 +0600

    add headers

--- main.cpp
+++ main.cpp
@@ -1,43 +1,4 @@
+// All Headers
 #include <bits/stdc++.h>
 using namespace std;
 
-struct GPA {
-    double grade;
-    double creditHour;
-};
-
-double calculateCGPA(double calculatedCG) {
-    return 0.00;
-}
-
-int main() {
-    cout << "Welcome to Easy CGPA Calculator. \n Calculate your CGPA here" << endl;
-
-    struct GPA gpa;
-
-    cout << "Enter number of Semester: ";
-    int totalSemester, totalCourses;
-    cin >> totalSemester;
-
-    while(totalSemester--) {
-        cout << "Enter number of Courses: ";
-        cin >> totalCourses;
-
-        for(int i=1; i<=totalCourses; i++) {
-            cout << "Grade and Credit Hour of Course " << i << " = "; 
-            cin >> gpa.grade >> gpa.creditHour;
-        }
-        
-    }
-
-    for(int i=1; i<=totalSemester; i++) {
-        cout << "Your Result in Semester " << i << ": "; 
-        for(int i=1; i<=totalCourses; i++) {
-            cout << "Course " << i << ", " << "Grade" << gpa.grade << ", " << "Credit Hour " << gpa.creditHour << ", " << "CGPA : " << calculateCGPA(5.00) << endl;;
-            cin >> gpa.grade >> gpa.creditHour;
-        }
-        
-    }
-
-    return 0;
-}
